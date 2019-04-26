c029c16c7cf9f79bfe0cea21ff2bc87de292cf96
Bartosz Zbislawski <bzbislawski@gmail.com>
2019-04-27 00:22:01 +0100

    Generate data

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,7 +5,7 @@
         <table>
             <tr v-for="chunk in items">
                 <td v-for="item in chunk">
-                    <span v-if="item.alive">x</span>
+                    <span >{{item}}</span>
                 </td>
             </tr>
         </table>
@@ -18,20 +18,34 @@
         data() {
             return {
                 items: [],
-            }
+                rowLength: 10}
         },
         mounted() {
-            this.items = [
-                {"id": 1, "left": null, "right": 2, "alive": false},
-                {"id": 2, "left": 1, "right": 3, "alive": false},
-                {"id": 3, "left": 2, "right": 4, "alive": false},
-                {"id": 4, "left": 3, "right": 5, "alive": false},
-                {"id": 5, "left": 4, "right": null, "alive": false},
-            ];
+            for (let i = 1; i <= this.rowLength * this.rowLength; i++) {
+                let neighbour0 = ((i - 1) % this.rowLength === 0) || i < this.rowLength + 1 ? null : i - this.rowLength - 1;
+                let neighbour1 = (i - this.rowLength > 0) ? i - this.rowLength :  null;
+                let neighbour2 = (i % this.rowLength === 0) || i <= this.rowLength - 1 ? null : i - this.rowLength + 1;
+                let neighbour3 = (i % this.rowLength === 0) ? null : i + 1;
+                let neighbour4 = (i + this.rowLength > this.rowLength * this.rowLength) || (i % this.rowLength === 0) ? null : i + this.rowLength - 1;
+                let neighbour5 = (i + this.rowLength <= this.rowLength * this.rowLength) ? i + this.rowLength : null;
+                let neighbour6 = (i + this.rowLength > this.rowLength * this.rowLength) || ((i - 1) % this.rowLength === 0) ? null : i + this.rowLength - 1;
+                let neighbour7 = ((i - 1) % this.rowLength === 0) ? null : i - 1;
+                this.items.push({
+                    "id": i,
+                    "neighbour0": neighbour0,
+                    "neighbour1": neighbour1,
+                    "neighbour2": neighbour2,
+                    "neighbour3": neighbour3,
+                    "neighbour4": neighbour4,
+                    "neighbour5": neighbour5,
+                    "neighbour6": neighbour6,
+                    "neighbour7": neighbour7,
+                });
+            }
 
             let arrays = [];
             while (this.items.length > 0)
-                arrays.push(this.items.splice(0, 10));
+                arrays.push(this.items.splice(0, this.rowLength));
 
             this.items = arrays;
 
